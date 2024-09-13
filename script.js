3a97d4223dc64eb7134439ad944ae739b53dc834
mast8 <ronald.miranda.rodriguez@gmail.com>
2024-09-13 11:34:31 -0400

    total counts

--- script.js
+++ script.js
@@ -2,35 +2,20 @@ let coin = document.getElementById("flip-coin");
 let flip = document.getElementById("flip-again");
 let tossResult = document.getElementById("result");
 
-/* function flipAgain() {
-    location.reload();
-} */
 
 coin.addEventListener("click", function () {
-    //let toss = Math.floor(Math.random() * 2);
+   
     flipCoin();
-    /* let result;
-    let tails, heads = 0;
-    
-    if (toss == 1) {
-        tails++;
-        result = "Tails";
-        coin.src = "images/Tails.png"
-    } else {
-        heads++;
-        coin.src = "images/Heads.png"
-        result = "Heads";
-    }
-
-    tossResult.innerHTML = `It's ${result}.  ${tails} and ${heads} `
-    flip.innerHTML = `<button id="flip-again-btn">Want to Flip Again?</button>`
-    flip.addEventListener("click", flipAgain); */
 });
 
 let tails =0  ;                                                 
 let heads = 0;
 
 function flipCoin (){
+    let tailsE = document.getElementById("tail");
+    let headsE = document.getElementById("head");
+    let totalE = document.getElementById("total");
+
     let result;
     let toss = Math.floor(Math.random() * 2);
     
@@ -45,7 +30,10 @@ function flipCoin (){
     }
     let total = heads +tails;
 
-    tossResult.innerHTML = `It's ${result}. Heads: ${tails} Tails: ${heads} total ${total} `
+    tossResult.innerHTML = `It's ${result}. Heads: ${tails} Tails: ${heads} total ${total} `;
+    headsE.innerHTML = `${heads} `;
+    tailsE.innerHTML = `${tails} `;
+    totalE.innerHTML = `${total} `; 
     flip.innerHTML = `<button id="flip-again-btn">Want to Flip Again?</button>`
     flip.addEventListener("click", flipCoin); 
 }
